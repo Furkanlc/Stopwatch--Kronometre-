7720333f2e21f6343c791431d6bd86b99fd8e1e8
Furkan GÜLEÇ <gulecf335@gmail.com>
2022-04-10 17:31:45 +0300

    first commit

---
+++ style.js
@@ -0,0 +1,43 @@
+let second = 0;
+let tens = 0;
+let interval;
+
+let getSeconds = document.querySelector(".second");
+let getTens = document.querySelector(".tens");
+
+let btnStart = document.querySelector(".btnStart");
+let btnStop = document.querySelector(".btnStop");
+let btnReset = document.querySelector(".btnReset");
+
+btnStart.addEventListener("click", () => {
+  interval = setInterval(timer, 10);
+});
+btnStop.addEventListener("click", () => {
+  clearInterval(interval);
+});
+
+btnReset.addEventListener("click", () => {
+  tens = "00";
+  second = "00";
+  getSeconds.innerHTML = second;
+  getTens.innerHTML = tens;
+});
+
+function timer() {
+  tens++;
+  if (tens <= 9) {
+    getTens.innerHTML = "0" + tens;
+  }
+  if (tens > 9) {
+    getTens.innerHTML = tens;
+  }
+  if (tens > 99) {
+    second++;
+    getSeconds.innerHTML = "0" + second;
+    tens = 0;
+    getTens.innerHTML = "0" + tens;
+  }
+  if (second > 9) {
+    getSeconds.innerHTML = second;
+  }
+}
